4339cd6360991431d2a47a668c3aaf0cde448901
猫とかまぼこ <67220994+kiyo-tech-git@users.noreply.github.com>
2023-07-19 23:13:58 +0900

    -

---
+++ operator_prac.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := 3
+
+	fmt.Println(x + y) // 13
+	fmt.Println(x - y) // 7
+	fmt.Println(x * y) // 30
+	fmt.Println(x / y) // 3
+	fmt.Println(x % y) // 1
+}
