ea4b221d811c9ac3156fc0d561a055595f4733a4
猫とかまぼこ <67220994+kiyo-tech-git@users.noreply.github.com>
2023-07-19 23:15:43 +0900

    -

--- operator_prac.go
+++ operator_prac.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// +,-,*,/,%
 	x := 10
 	y := 3
 
@@ -11,4 +12,9 @@ func main() {
 	fmt.Println(x * y) // 30
 	fmt.Println(x / y) // 3
 	fmt.Println(x % y) // 1
+
+	// relationship
+	fmt.Println(x > y) // true
+	fmt.Println(x < y) // false
+
 }
