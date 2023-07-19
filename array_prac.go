1e3a6e64070936d390b77a7e3ef0cfb706ca7936
猫とかまぼこ <67220994+kiyo-tech-git@users.noreply.github.com>
2023-07-19 22:59:00 +0900

    array

---
+++ array_prac.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// how to create an array
+	a := [3]string{"A", "B", "C"}
+
+	fmt.Println(a[0]) //A
+	fmt.Println(a[1]) //B
+	fmt.Println(a[2]) //C
+}
